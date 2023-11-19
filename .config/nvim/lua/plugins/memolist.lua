cf61b8d129e6dcf95dabf0ff8b1bc232b525fd11
SubTaro <73282880+SubTaro@users.noreply.github.com>
2023-11-19 20:50:51 +0900

    auto save

--- .config/nvim/lua/plugins/memolist.lua
+++ .config/nvim/lua/plugins/memolist.lua
@@ -7,9 +7,9 @@ return {
 		{"<leader>mg", "<cmd>MemoGrep<CR>"},
 	},
 	config = function()
-		vim.g["memolist_path"] = "/home/subtaro/.memolist/memo"
+		vim.g["memolist_path"] = "~/.memolist/memo"
 		vim.g["memolist_memo_suffix"] = "md"
 		vim.g["memolist_fzf"] = 1
-		vim.g["memolist_template_dir_path"] = "~/.memolist/memotemplates/md.txt"
+		vim.g["memolist_template_dir_path"] = "~/.memolist/memotemplates"
 	end,
 }
